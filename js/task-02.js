27f0eeb0b9d6f361e98caecd5068bf739b19de48
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-07-23 09:57:10 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const arrItem = [];
+
+const addItems = (arr) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    const item = document.createElement('li');
+    item.textContent = ingredients[i];
+    item.classList.add('item');
+    arrItem.push(item)
+  }
+  list.append(...arrItem)
+}
+
+addItems(ingredients)

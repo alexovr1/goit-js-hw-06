0642069dcc2b9005a8336e24eb9b68ef2026a650
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-07-23 22:51:12 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+}
+
+const onChangeColorBody = () => {
+  const changedColor = getRandomHexColor();
+  refs.body.style.backgroundColor = changedColor;
+  refs.span.textContent = changedColor;
+}
+
+refs.btn.addEventListener('click', onChangeColorBody)

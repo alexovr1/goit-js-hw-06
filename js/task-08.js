7d1f6339227c5e2e2aba98814a1093e1a98a217f
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-07-23 16:48:18 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form')
+console.log(formRef);
 
+const onVerificatForm = (e) => {
+    e.preventDefault();
+    const {
+        elements: { email, password }
+    } = e.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+    else {
+        const user = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(user);
+    }
+    formRef.reset();
+}
+
+formRef.addEventListener('submit', onVerificatForm)

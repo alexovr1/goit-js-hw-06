b301d56c72a5baad8b59e8edabb2d47ad4faba8d
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-07-23 15:28:29 +0300

    task5

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+
+
+// console.log(textInput);
+
+textInput.addEventListener("input", (event) => {
+    output.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === '') {
+        return output.textContent = 'Anonymous';
+    }
+});

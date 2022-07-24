9e8b8df79345b46aa7478d2121821e52bc8b1a40
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-07-24 21:24:17 +0300

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,8 @@
 const listEl = document.querySelector('#categories');
-
-const titletextEl = (number) =>
-    listEl.children[number].children[0].textContent;
-
-const elementsOfCategory = (number) =>
-    listEl.children[number].children[1].childElementCount;
-
+const itemEl = document.querySelectorAll('.item')
 
 console.log(`Number of categories: ${listEl.childElementCount}`);
 
-console.log(`Category: ${titletextEl(0)}`);
-console.log(`Elements: ${elementsOfCategory(0)}`);
-
-console.log(`Category: ${titletextEl(1)}`);
-console.log(`Elements: ${elementsOfCategory(1)}`);
-
-console.log(`Category: ${titletextEl(2)}`);
-console.log(`Elements: ${elementsOfCategory(2)}`);
+itemEl.forEach((element) => {
+    console.log(`Category: ${element.firstElementChild.textContent}`), console.log(`Elements: ${element.children[1].childElementCount}`);
+});

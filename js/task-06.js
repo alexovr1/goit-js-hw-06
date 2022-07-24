8510b61c2cfafd2599b86f965b23a62f5161a456
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-07-24 10:29:59 +0300

    +

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,13 @@
 const inputRef = document.querySelector('#validation-input');
 
 const onSwitchFocusInput = () => {
-    if (inputRef.value.length === inputRef.dataset.length) {
+    if (inputRef.value.length === +inputRef.dataset.length) {
         inputRef.classList.remove('invalid')
         inputRef.classList.add('valid');
     } else {
         inputRef.classList.remove('valid');
         inputRef.classList.add('invalid');
-
     };
 }
 
-inputRef.addEventListener('change', onSwitchFocusInput)
-
+inputRef.addEventListener('change', onSwitchFocusInput);
